49b3ee5baf4e26ded28cfe56c8085cc203498aa7
ofcZsgj <tcuZsgj@gmail.com>
2021-01-12 23:38:26 +0800

    Add noncopyable class

---
+++ noncopyable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * noncopyablel 被继承后，派生类对象能够正常的构造和析构，但是无法进行拷贝和赋值
+ */
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable &) = delete;
+    noncopyable &operator=(const noncopyable &) = delete;
+
+protected:
+    noncopyable();
+    ~noncopyable();
+};
