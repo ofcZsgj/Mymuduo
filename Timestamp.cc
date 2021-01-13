56b729c1c349e2c88ac07ed3df9cc11b20c8e7fe
ofcZsgj <tcuZsgj@gmail.com>
2021-01-13 20:42:28 +0800

    Add Timestamp class

---
+++ Timestamp.cc
@@ -0,0 +1,27 @@
+#include "Timestamp.h"
+
+#include <time.h>
+
+Timestamp::Timestamp(int64_t microSecondsSinceEpochArg)
+    : microSecondsSinceEpochArg_(microSecondsSinceEpochArg)
+{
+}
+
+Timestamp Timestamp::now()
+{
+    return Timestamp(time(NULL));
+}
+
+std::string Timestamp::toString() const
+{
+    char buf[128] = {0};
+    tm *tm_time = localtime(&microSecondsSinceEpochArg_);
+    snprintf(buf, 128, "%4d/%02d/%02d %02d:%02d:%02d",
+             tm_time->tm_year + 1900,
+             tm_time->tm_mon + 1,
+             tm_time->tm_mday,
+             tm_time->tm_hour,
+             tm_time->tm_min,
+             tm_time->tm_sec);
+    return buf;
+}
---
+++ Timestamp.h
@@ -0,0 +1,18 @@
+#pragma
+
+#include <iostream>
+#include <string>
+
+class Timestamp
+{
+public:
+    Timestamp() : microSecondsSinceEpochArg_(0)
+    {
+    }
+    explicit Timestamp(int64_t microSecondsSinceEpochArg);
+    static Timestamp now();
+    std::string toString() const;
+
+private:
+    int64_t microSecondsSinceEpochArg_;
+};
