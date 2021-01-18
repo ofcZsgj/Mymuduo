9e791ae27607d5110cd68b24ba16cd07c0a26e41
ofcZsgj <tcuZsgj@gmail.com>
2021-01-18 22:39:18 +0800

    Package socket api

---
+++ Socket.cc
@@ -0,0 +1,79 @@
+#include "Socket.h"
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "InetAddress.h"
+#include "Logger.h"
+#include "strings.h"
+
+Socket::~Socket()
+{
+    ::close(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress &localaddr)
+{
+    if (0 != ::bind(sockfd_, (sockaddr *)localaddr.getSockAddr(), sizeof(sockaddr_in)))
+    {
+        LOG_FATAL("bind sockfd%d fail\n", sockfd_);
+    }
+}
+
+void Socket::listen()
+{
+    if (0 != ::listen(sockfd_, 1024))
+    {
+        LOG_FATAL("listen sockfd%d fail\n", sockfd_);
+    }
+}
+
+int Socket::accept(InetAddress *peeraddr)
+{
+    sockaddr_in addr;
+    socklen_t len;
+    bzero(&addr, sizeof addr);
+    int connfd = ::accept(sockfd_, (sockaddr *)&addr, &len);
+    if (connfd > 0)
+    {
+        peeraddr->setSockaddr(addr);
+    }
+    return connfd;
+}
+
+void Socket::shutdownWrite(bool on)
+{
+    if (::shutdown(sockfd_, SHUT_WR) < 0)
+    {
+        LOG_ERROR("shutdownWrite error\n");
+    }
+}
+
+void Socket::setTcpNoDelay(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY,
+                 &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::setReuseAddr(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR,
+                 &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::setReusePort(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT,
+                 &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::setKeepAlive(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE,
+                 &optval, static_cast<socklen_t>(sizeof optval));
+}
